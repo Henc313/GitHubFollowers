74fa71375e56a81db750c73147715b419675fe13
Henry <henc313@gmail.com>
2020-01-04 13:46:23 +0200

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by ♏︎ on 1/4/20.
+//  Copyright © 2020 Henry Kivimaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
