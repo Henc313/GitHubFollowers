806161a2d124114f8589360485eb9618efab9b45
Henry <henc313@gmail.com>
2020-01-25 09:38:53 +0200

    Create a network call for downloading avatar images

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2020 Henry Kivimaa. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
-   static let shared = NetworkManager()
-   let baseURL       = "https://api.github.com/users/"
+   static let shared   = NetworkManager()
+   private let baseURL = "https://api.github.com/users/"
+   let cache           = NSCache<NSString, UIImage>()
    
    private init() {}
    
