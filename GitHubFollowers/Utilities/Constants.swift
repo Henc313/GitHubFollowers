ac13c20dbe3c34ab98868068502a88e9d644b853
Henry <henc313@gmail.com>
2020-02-01 18:32:06 +0200

    Create UserInfo ViewController Create SecondaryTitleLabel Configure UserInfo ViewController Create Constants enum

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by ♏︎ on 2/1/20.
+//  Copyright © 2020 Henry Kivimaa. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+   static let location = "mappin.and.ellipse"
+}
