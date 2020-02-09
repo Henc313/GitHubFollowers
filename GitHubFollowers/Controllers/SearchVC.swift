f86945979c442e6c7bab3ea05dbf46ed42e777db
Henry <henc313@gmail.com>
2020-02-09 16:24:22 +0200

    Create TabBarController Add size classes to constants file Use size classes to configure constraints on smaller displays Refactor favorites and followers

--- GitHubFollowers/Controllers/SearchVC.swift
+++ GitHubFollowers/Controllers/SearchVC.swift
@@ -13,6 +13,7 @@ class SearchVC: UIViewController {
    let logoImageView      = UIImageView()
    let usernameTextField  = GFTextField()
    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+   var logoImageViewTopConstraint: NSLayoutConstraint!
    
    var isUsernameEntered: Bool { return !usernameTextField.text!.isEmpty }
    
@@ -29,6 +30,7 @@ class SearchVC: UIViewController {
    
    override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
+      usernameTextField.text = ""
       navigationController?.setNavigationBarHidden(true, animated: true)   }
    
    func createDismissKeyboardTapGesture() {
@@ -43,7 +45,9 @@ class SearchVC: UIViewController {
          return
       }
       
-      let followerListVC      = FollowerListVC()
+      usernameTextField.resignFirstResponder()
+      
+      let followerListVC      = FollowerListVC(username: usernameTextField.text!)
       followerListVC.username = usernameTextField.text
       followerListVC.title    = usernameTextField.text
       navigationController?.pushViewController(followerListVC, animated: true)
@@ -52,10 +56,14 @@ class SearchVC: UIViewController {
    func configureLogoImageView() {
       view.addSubview(logoImageView)
       logoImageView.translatesAutoresizingMaskIntoConstraints = false
-      logoImageView.image = UIImage(named: "gh-logo")
+      logoImageView.image = Images.ghLogo
+      
+      let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 20 : 80
+      
+      logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
+      logoImageViewTopConstraint.isActive = true
       
       NSLayoutConstraint.activate([
-         logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
          logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
          logoImageView.heightAnchor.constraint(equalToConstant: 200),
          logoImageView.widthAnchor.constraint(equalToConstant: 200)
---
+++ GitHubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  GFTabBarController.swift
+//  GitHubFollowers
+//
+//  Created by ♏︎ on 2/9/20.
+//  Copyright © 2020 Henry Kivimaa. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      UITabBar.appearance().tintColor = .systemGreen
+      self.viewControllers            = [createSearchNC(), createFavoritesNC()]
+   }
+   
+   
+   func createSearchNC() -> UINavigationController {
+      let searchVC        = SearchVC()
+      searchVC.title      = "Search"
+      searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+      
+      return UINavigationController(rootViewController: searchVC)
+   }
+   
+   
+   func createFavoritesNC() -> UINavigationController {
+      let favoritesListVC        = FavoritesListVC()
+      favoritesListVC.title      = "Favorites"
+      favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+      
+      return UINavigationController(rootViewController: favoritesListVC)
+   }
+   
+   
+}
